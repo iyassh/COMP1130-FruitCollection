b6c0dd4c615786cbe29c9280d04caf79e9e02beb
Yassh Singh <90470603+iyassh@users.noreply.github.com>
2021-10-14 12:33:51 +0530

    Added weight variable

--- Guava.java
+++ Guava.java
@@ -1,6 +1,19 @@
 
+
 public class Guava {
+    private double weight_guava;
+
     public String toString() {
         return "This is a Guava created by Yassh";
+    }
+
+    // Getter
+    public double getGuavaWeight() {
+        return weight_guava;
+    }
+
+    //Setter
+    public void setGuavaWeight(double newWeight) {
+        this.weight_guava = newWeight;
     }
 }
